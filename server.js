ccf18137db46b1a038a8ac36c02eb9bfa260ed62
Ashkon Honardoost <ashkon91@gmail.com>
2017-02-08 23:39:12 -0800

    port change

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 8080;
 
 //app.use(express.static(__dirname + '/public'));
 app.use('/', express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     console.log("Listening on port %s...", server.address().port);
 });
 
