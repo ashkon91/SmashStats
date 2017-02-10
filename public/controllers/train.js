ef5168465e4973558d2e19c9a5366085badb061d
Ashkon Honardoost <ashkon91@gmail.com>
2017-02-09 22:02:21 -0800

    Angular buttons figured out

--- public/controllers/train.js
+++ public/controllers/train.js
@@ -2,8 +2,8 @@ angular.module('SmashStats')
 .controller('TrainCtrl', function($rootScope, $scope) {
 	$rootScope.activePage = 'train';
 	$scope.difficultySelected = "Choose your difficulty";
-	$scope.diffClicked = function() {
-		console.log("TEST");
-		$scope.difficultySelected = "BUTTON CLICKED";
+	$scope.diffClicked = function(val) {
+		console.log(val);
+		$scope.difficultySelected = val + " difficulty routine chosen.";
 	}
 });
