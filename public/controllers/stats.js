84bde45b37c5aa05eefe82d19cb158654d029b19
Ashkon Honardoost <ashkon91@gmail.com>
2017-02-15 22:12:58 -0800

    Worked on stats page

--- public/controllers/stats.js
+++ public/controllers/stats.js
@@ -1,6 +1,18 @@
 angular.module('SmashStats')
-.controller('StatsCtrl', function($rootScope, $scope) {
+.controller('StatsCtrl', function($rootScope, $scope, $http) {
 	$rootScope.activePage = 'stats';
+	$http.get('../data.json')
+	.then(function(data){
+			$scope.names = [];
+			for(val in data.data.Name){
+				$scope.names.push(val);
+			}
+			
+			console.log(data.data.Name["Ashkon"].Character["Fox"].Stage["Battlefield"]);
+			$scope.wins = data.data.Name["Ashkon"].Character["Fox"].Stage["Battlefield"].wins;
+			$scope.losses = data.data.Name["Ashkon"].Character["Fox"].Stage["Battlefield"].losses;
+			$scope.someshit = "THUIEJOPKAKSJFBOPSNAM";
 
+	})
 
 });
