a4ec977222a3d351e7482866ccbb20670c0cbbc5
Harold Shen <hlshen@ucsd.edu>
2017-02-23 22:24:54 -0800

    added logout button

--- public/controllers/settings.js
+++ public/controllers/settings.js
@@ -1,6 +1,9 @@
 angular.module('SmashStats')
-.controller('SettingsCtrl', function($rootScope, $scope) {
+.controller('SettingsCtrl', function($rootScope, $scope, $state) {
 	$rootScope.activePage = 'settings';
 
-
+	$scope.logout = function(){
+		$rootScope.isLoggedIn = false;
+		$state.go('login');
+	}
 });
