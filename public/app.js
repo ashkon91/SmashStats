d1c73b381fe53f6d5a148424f5cc5273e02a8ba3
Harold Shen <hlshen@ucsd.edu>
2017-03-06 18:38:26 -0800

    Readded experiment code

--- public/app.js
+++ public/app.js
@@ -98,6 +98,6 @@ angular
 .config(['AnalyticsProvider', function (AnalyticsProvider) {
    // Add configuration code as desired
    AnalyticsProvider.setAccount('UA-93163212-1');  //UU-XXXXXXX-X should be your tracking code
-   AnalyticsProvider.setExperimentId('OqTl88LgR5iVy_5sOCXbJg');
+   //AnalyticsProvider.setExperimentId('OqTl88LgR5iVy_5sOCXbJg');
    // key : 142131724-0
 }]).run(['Analytics', function(Analytics) { }]);
